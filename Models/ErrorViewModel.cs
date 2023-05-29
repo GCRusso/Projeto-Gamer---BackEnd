efafff4e163218368fcf6dd2e4062b9257edc886
Gabriel Coral Russo <23655858841@info.local>
2023-05-29 08:24:04 -0300

    inicio de projeto, fazendo a estrutura do código

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_Gamer___BackEnd.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
