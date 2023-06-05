e5fe2be8d906c615748c4e26538f292aedf93dc4
Gabriel Coral Russo <23655858841@info.local>
2023-06-05 08:52:56 -0300

    adicionado a parte de front de Cadastro de Jogadores, falta a parte lógica para Cadastrar

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Projeto_Gamer___BackEnd.Infra;
+using Projeto_Gamer___BackEnd.Models;
 
 namespace Projeto_Gamer___BackEnd.Controllers
 {
@@ -13,8 +14,9 @@ namespace Projeto_Gamer___BackEnd.Controllers
             _logger = logger;
         }
         //instanciando o objeto do banco de dados, para utilizar o banco de dados.
-        Context c = new Context();
         
+        //TODO Método Listar
+        Context c = new Context();
         [Route("Listar")]
         public IActionResult Index()
         {
@@ -23,6 +25,32 @@ namespace Projeto_Gamer___BackEnd.Controllers
 
             return View();
         }
+
+        //TODO Método Cadastrar
+        [Route("Cadastrar")]
+
+        public IActionResult Cadastrar (IFormCollection form)
+        {
+            Jogador novoJogador = new Jogador();
+
+            novoJogador.IdJogador = 0;
+            novoJogador.NomeJogador = Console.ReadLine();
+            novoJogador.Email = Console.ReadLine();
+            novoJogador.Senha = Console.ReadLine();
+            
+            c.Jogador.Add(novoJogador);
+            c.SaveChanges();
+            return LocalRedirect("~/Equipe/Listar");
+            
+        }
+
+
+
+
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
